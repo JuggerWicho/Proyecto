26aa1e4545b580e293ba37e92c8ea3cbd771a71a
Alan Amador <alan_reto__99@hotmail.com>
2016-02-27 13:11:30 -0600

    cambio 3
    antes de salir de clase

--- tamagotchi.cpp
+++ tamagotchi.cpp
@@ -10,9 +10,10 @@ class tamagotchi{
 		void setpeso(float Ppeso);
 		float getpeso(void);
 		void setcolor( string Ccolor);
-		string getcolor(void);
+		string getcolor(void); 
+
 		//MEDIDORES
-	/*	void setfelic ( int Ffelic);
+		void setfelic ( int Ffelic);
 		int getfelic(void);
 		void setdinero(int Ddinero);
 		int getdinero (void);
@@ -20,15 +21,18 @@ class tamagotchi{
 		int gethambre (void);
 		void setaburr(int Aaburr);
 		int getaburr(void);
-		void setsueño(int Ssueño);
+		void setsueño(int Ssueno);
 		int getsueño(void);
 		void setsalud(int Ssalud);
 		int getsalud(void);
+		tamagotchi::tamagotchi();
+		tamagotchi::~tamagotchi();
+		
 		//METODOS
 		void acariciar();
 		void alimentar();
 		void entrenar();
-		void bañar();  */
+		void bañar();  
 		
 	private:
 		//ATRIBUTOS
@@ -37,13 +41,13 @@ class tamagotchi{
 		float peso;
 		int edad;
 		//MEDIDORES
-	/*	int felic;
+	  int felic;
 		int dinero;
 		int hambre;
 		int aburr;
-		int sueño;
-		int salud; */
-};
+		int sueno;
+		int salud; 
+}; 
 //SET
 	void tamagotchi:: setnombre(string Nnombre){
 		nombre = Nnombre;		
@@ -70,6 +74,19 @@ class tamagotchi{
 	string tamagotchi::getcolor(void){
 		return color;
 	}
+	
+	 tamagotchi::tamagotchi(){
+		nombre = "SinNombre";
+		edad = 0;
+		peso = 0 ;
+		color = "SinColor";
+		dinero = 100;
+		felic = 75;
+		hambre = 25;
+		aburr = 25;
+		sueno = 75;
+		salud = 100;
+	} 
 int main(){
 	tamagotchi mascota;
 	string nombree;
@@ -89,6 +106,19 @@ int main(){
 	cin>>coloor;
 	mascota.setcolor(coloor);
 	cout<<"Esto son los datos de tu tamagotchi: "<<endl<<"Nombre: "<<mascota.getnombre()<<endl<<"Edad: "<<mascota.getedad()<<endl<<"Peso: "<<mascota.getpeso()<<endl<<"Color: "<<mascota.getcolor();
-	return 0;
+	cout<<"Que deseas hacer";
+	cout<<"- - - - - -- - -  - - - - - - - - - - - - - - - - - - - ";
+	cout<<"|           1.Alimentar...                             |";
+	cout<<"|                                                      |";
+	cout<<"|           2.Bañar                                    |";
+	cout<<"|                                                      |";
+	cout<<"|           3.Acariciar                                |";
+	cout<<"|                                                      |";
+	cout<<"|           4.Entrenar                                 |";
+	cout<<"|                                                      |";
+	cout<<"|           5.Jugar                                    |";
+	cout<<"|                                                      |";
+	cout<<"|___________6.Dormir___________________________________|";
+		return 0;
 }
 
